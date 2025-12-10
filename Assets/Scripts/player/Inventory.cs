63466854ca8083de4a30273d5ce991b547fd6d8b
Alexander Gréus <alexgreus51@gmail.com>
2025-12-09 20:22:10 -0600

    store code layout + first gui

---
+++ Assets/Scripts/player/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using store.logic;
+
+namespace player
+{
+    public class Inventory
+    {
+        
+        private List<Item> items = new List<Item>();
+        
+        public List<Item> getItems()
+        {
+            return items;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/store/logic/Store.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace store.logic
+{
+    public class Store
+    {
+        
+        List<Item> items;
+
+        public void initialize()
+        {
+            items = new List<Item>();
+            
+            addItem(new Item("Test1", 10.5f));
+            addItem(new Item("Test2", 5.0f));
+        }
+
+        private void addItem(Item item)
+        {
+            items.Add(item);
+        }
+        
+        public List<Item> GetItems()
+        {
+            return items;
+        }
+        
+        
+        
+    }
+}
